7d446cff76bf41282f0a3562fe8613c839656dc7
Kristiyan9359 <kristiyankamboshev@gmail.com>
2026-07-22 16:56:33 +0300

    Add application role constants

---
+++ KiddoCare.Common/RoleConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace KiddoCare.Common;
+
+public static class RoleConstants
+{
+    public const string Admin = "Admin";
+    public const string Teacher = "Teacher";
+    public const string Parent = "Parent";
+}
