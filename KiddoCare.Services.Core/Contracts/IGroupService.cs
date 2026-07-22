af9ea8f100ef8b3c740d9cef222031d6d7df355f
Kristiyan9359 <kristiyankamboshev@gmail.com>
2026-07-22 18:01:27 +0300

    Add group service contract

---
+++ KiddoCare.Services.Core/Contracts/IGroupService.cs
@@ -0,0 +1,16 @@
+﻿using KiddoCare.ViewModels.Groups;
+
+namespace KiddoCare.Services.Core.Contracts;
+
+public interface IGroupService
+{
+    Task<IEnumerable<GroupIndexViewModel>> GetAllAsync();
+
+    Task<GroupEditViewModel?> GetForEditAsync(int id);
+
+    Task CreateAsync(GroupCreateViewModel model);
+
+    Task EditAsync(GroupEditViewModel model);
+
+    Task DeleteAsync(int id);
+}
