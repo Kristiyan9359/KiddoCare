5142678a40fcce782cf70b0b33717e21395592a5
Kristiyan9359 <kristiyankamboshev@gmail.com>
2026-07-22 17:32:09 +0300

    Add kindergarten group entity

---
+++ KiddoCare.Common/ValidationConstants.cs
@@ -0,0 +1,7 @@
+﻿namespace KiddoCare.Common;
+
+public static class ValidationConstants
+{
+    public const int KindergartenGroupNameMaxLength = 80;
+    public const int KindergartenGroupDescriptionMaxLength = 500;
+}
---
+++ KiddoCare.Data.Models/KindergartenGroup.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static KiddoCare.Common.ValidationConstants;
+
+namespace KiddoCare.Data.Models;
+
+public class KindergartenGroup
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(KindergartenGroupNameMaxLength)]
+    public string Name { get; set; } = null!;
+
+    [MaxLength(KindergartenGroupDescriptionMaxLength)]
+    public string? Description { get; set; }
+
+    public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
+
+    public bool IsDeleted { get; set; }
+}
