9abfe5958d091727d47c84a68090c8c5997d8581
Kristiyan9359 <kristiyankamboshev@gmail.com>
2026-07-24 18:50:07 +0300

    Add attendance controller

---
+++ KiddoCare.Web/Controllers/AttendanceController.cs
@@ -0,0 +1,46 @@
+﻿using KiddoCare.Services.Core.Contracts;
+using KiddoCare.ViewModels.Attendance;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using static KiddoCare.Common.RoleConstants;
+
+namespace KiddoCare.Web.Controllers;
+
+[Authorize(Roles = $"{Admin},{Teacher}")]
+public class AttendanceController : Controller
+{
+    private readonly IAttendanceService attendanceService;
+
+    public AttendanceController(IAttendanceService attendanceService)
+    {
+        this.attendanceService = attendanceService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Daily(DateTime? date, int? groupId)
+    {
+        var selectedDate = date ?? DateTime.Today;
+
+        var model = await attendanceService.GetDailyAttendanceAsync(selectedDate, groupId);
+
+        return View(model);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Daily(AttendanceDailyViewModel model)
+    {
+        if (!ModelState.IsValid)
+        {
+            model = await attendanceService.GetDailyAttendanceAsync(model.Date, model.GroupId);
+            return View(model);
+        }
+
+        await attendanceService.SaveDailyAttendanceAsync(model);
+
+        return RedirectToAction(nameof(Daily), new
+        {
+            date = model.Date.ToString("yyyy-MM-dd"),
+            groupId = model.GroupId
+        });
+    }
+}
