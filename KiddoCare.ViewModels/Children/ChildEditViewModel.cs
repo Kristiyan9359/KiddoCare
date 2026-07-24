5e6488504a22ede5404f166eae3aa15ed2a8d326
Kristiyan9359 <kristiyankamboshev@gmail.com>
2026-07-24 16:53:17 +0300

    Add child view models

---
+++ KiddoCare.ViewModels/Children/ChildEditViewModel.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+using static KiddoCare.Common.ValidationConstants;
+
+namespace KiddoCare.ViewModels.Children;
+
+public class ChildEditViewModel
+{
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(ChildFirstNameMaxLength)]
+    public string FirstName { get; set; } = null!;
+
+    [Required]
+    [MaxLength(ChildLastNameMaxLength)]
+    public string LastName { get; set; } = null!;
+
+    [Required]
+    public DateTime DateOfBirth { get; set; }
+
+    [MaxLength(ChildAllergiesMaxLength)]
+    public string? Allergies { get; set; }
+
+    [MaxLength(ChildAdditionalNotesMaxLength)]
+    public string? AdditionalNotes { get; set; }
+
+    [Required]
+    public int GroupId { get; set; }
+
+    public IEnumerable<SelectListItem> Groups { get; set; } = new List<SelectListItem>();
+}
---
+++ KiddoCare.ViewModels/Children/ChildIndexViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace KiddoCare.ViewModels.Children;
+
+public class ChildIndexViewModel
+{
+    public int Id { get; set; }
+
+    public string FullName { get; set; } = null!;
+
+    public DateTime DateOfBirth { get; set; }
+
+    public string GroupName { get; set; } = null!;
+}
