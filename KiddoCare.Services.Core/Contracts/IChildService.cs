5fec2685f9d904da54b0019ef2286f5bce231c56
Kristiyan9359 <kristiyankamboshev@gmail.com>
2026-07-24 16:54:03 +0300

    Add child service contract

---
+++ KiddoCare.Services.Core/Contracts/IChildService.cs
@@ -0,0 +1,18 @@
+﻿using KiddoCare.ViewModels.Children;
+
+namespace KiddoCare.Services.Core.Contracts;
+
+public interface IChildService
+{
+    Task<IEnumerable<ChildIndexViewModel>> GetAllAsync();
+
+    Task<ChildCreateViewModel> GetCreateModelAsync();
+
+    Task<ChildEditViewModel?> GetForEditAsync(int id);
+
+    Task CreateAsync(ChildCreateViewModel model);
+
+    Task EditAsync(ChildEditViewModel model);
+
+    Task DeleteAsync(int id);
+}
