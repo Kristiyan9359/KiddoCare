a4f0a400d2f1653625d504f3d3e42b8ce43f7675
Kristiyan9359 <kristiyankamboshev@gmail.com>
2026-07-24 18:48:35 +0300

    Add attendance service contract

---
+++ KiddoCare.Services.Core/Contracts/IAttendanceService.cs
@@ -0,0 +1,10 @@
+﻿using KiddoCare.ViewModels.Attendance;
+
+namespace KiddoCare.Services.Core.Contracts;
+
+public interface IAttendanceService
+{
+    Task<AttendanceDailyViewModel> GetDailyAttendanceAsync(DateTime date, int? groupId);
+
+    Task SaveDailyAttendanceAsync(AttendanceDailyViewModel model);
+}
