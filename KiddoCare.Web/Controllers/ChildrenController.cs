aa3d73b0d979a1783471d31c4bae798b16a13291
Kristiyan9359 <kristiyankamboshev@gmail.com>
2026-07-24 17:16:47 +0300

    Add children controller

---
+++ KiddoCare.Web/Controllers/ChildrenController.cs
@@ -0,0 +1,105 @@
+﻿using KiddoCare.Services.Core.Contracts;
+using KiddoCare.ViewModels.Children;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using static KiddoCare.Common.RoleConstants;
+
+namespace KiddoCare.Web.Controllers;
+
+[Authorize(Roles = Admin)]
+public class ChildrenController : Controller
+{
+    private readonly IChildService childService;
+
+    public ChildrenController(IChildService childService)
+    {
+        this.childService = childService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Index()
+    {
+        var children = await childService.GetAllAsync();
+
+        return View(children);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Create()
+    {
+        var model = await childService.GetCreateModelAsync();
+
+        return View(model);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(ChildCreateViewModel model)
+    {
+        if (!ModelState.IsValid)
+        {
+            model.Groups = (await childService.GetCreateModelAsync()).Groups;
+            return View(model);
+        }
+
+        await childService.CreateAsync(model);
+
+        return RedirectToAction(nameof(Index));
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Edit(int id)
+    {
+        var model = await childService.GetForEditAsync(id);
+
+        if (model == null)
+        {
+            return NotFound();
+        }
+
+        return View(model);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Edit(ChildEditViewModel model)
+    {
+        if (!ModelState.IsValid)
+        {
+            var editModel = await childService.GetForEditAsync(model.Id);
+
+            if (editModel == null)
+            {
+                return NotFound();
+            }
+
+            model.Groups = editModel.Groups;
+
+            return View(model);
+        }
+
+        try
+        {
+            await childService.EditAsync(model);
+        }
+        catch (InvalidOperationException)
+        {
+            return NotFound();
+        }
+
+        return RedirectToAction(nameof(Index));
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Delete(int id)
+    {
+        try
+        {
+            await childService.DeleteAsync(id);
+        }
+        catch (InvalidOperationException)
+        {
+            return NotFound();
+        }
+
+        return RedirectToAction(nameof(Index));
+    }
+}
