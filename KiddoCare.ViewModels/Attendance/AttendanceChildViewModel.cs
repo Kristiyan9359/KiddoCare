26b230e54216ebac814e5b1d80ef2a7f4155da77
Kristiyan9359 <kristiyankamboshev@gmail.com>
2026-07-24 18:47:54 +0300

    Add attendance view models

---
+++ KiddoCare.ViewModels/Attendance/AttendanceChildViewModel.cs
@@ -0,0 +1,14 @@
+﻿using KiddoCare.Data.Models.Enums;
+
+namespace KiddoCare.ViewModels.Attendance;
+
+public class AttendanceChildViewModel
+{
+    public int ChildId { get; set; }
+
+    public string FullName { get; set; } = null!;
+
+    public AttendanceStatus Status { get; set; } = AttendanceStatus.Present;
+
+    public string? Note { get; set; }
+}
---
+++ KiddoCare.ViewModels/Attendance/AttendanceDailyViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace KiddoCare.ViewModels.Attendance;
+
+public class AttendanceDailyViewModel
+{
+    public DateTime Date { get; set; } = DateTime.Today;
+
+    public int? GroupId { get; set; }
+
+    public IEnumerable<Microsoft.AspNetCore.Mvc.Rendering.SelectListItem> Groups { get; set; }
+        = new List<Microsoft.AspNetCore.Mvc.Rendering.SelectListItem>();
+
+    public IEnumerable<AttendanceChildViewModel> Children { get; set; }
+        = new List<AttendanceChildViewModel>();
+}
