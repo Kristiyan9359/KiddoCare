12abcee24162e312488eb3b8b08407f8394b1823
Kristiyan9359 <kristiyankamboshev@gmail.com>
2026-07-29 14:31:22 +0300

    Add dashboard view model and contract

---
+++ KiddoCare.Services.Core/Contracts/IDashboardService.cs
@@ -0,0 +1,8 @@
+﻿using KiddoCare.ViewModels.Dashboard;
+
+namespace KiddoCare.Services.Core.Contracts;
+
+public interface IDashboardService
+{
+    Task<DashboardViewModel> GetDashboardAsync();
+}
---
+++ KiddoCare.ViewModels/Dashboard/DashboardViewModel.cs
@@ -0,0 +1,22 @@
+﻿using KiddoCare.ViewModels.Events;
+
+namespace KiddoCare.ViewModels.Dashboard;
+
+public class DashboardViewModel
+{
+    public int GroupsCount { get; set; }
+
+    public int ChildrenCount { get; set; }
+
+    public int PresentTodayCount { get; set; }
+
+    public int AbsentTodayCount { get; set; }
+
+    public int SickTodayCount { get; set; }
+
+    public int LateTodayCount { get; set; }
+
+    public int VacationTodayCount { get; set; }
+
+    public IEnumerable<EventIndexViewModel> UpcomingEvents { get; set; } = new List<EventIndexViewModel>();
+}
