2be4f368ba8d285fbab345e868a1c7a6d383ff4d
Kristiyan9359 <kristiyankamboshev@gmail.com>
2026-07-29 14:54:37 +0300

    Add parent service contract

---
+++ KiddoCare.Services.Core/Contracts/IParentService.cs
@@ -0,0 +1,22 @@
+﻿using KiddoCare.ViewModels.Parents;
+
+namespace KiddoCare.Services.Core.Contracts;
+
+public interface IParentService
+{
+    Task<IEnumerable<ParentIndexViewModel>> GetAllAsync();
+
+    Task<ParentDetailsViewModel?> GetDetailsAsync(int id);
+
+    Task<ParentCreateViewModel> GetCreateModelAsync();
+
+    Task CreateAsync(ParentCreateViewModel model);
+
+    Task<ParentEditViewModel?> GetForEditAsync(int id);
+
+    Task EditAsync(ParentEditViewModel model);
+
+    Task<ParentDeleteViewModel?> GetForDeleteAsync(int id);
+
+    Task DeleteAsync(int id);
+}
