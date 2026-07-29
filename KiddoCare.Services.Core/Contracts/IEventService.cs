990c105f6b301810125504ce0efb2a68ee3ec3ab
Kristiyan9359 <kristiyankamboshev@gmail.com>
2026-07-29 14:20:35 +0300

    Add event service contract

---
+++ KiddoCare.Services.Core/Contracts/IEventService.cs
@@ -0,0 +1,20 @@
+﻿using KiddoCare.ViewModels.Events;
+
+namespace KiddoCare.Services.Core.Contracts;
+
+public interface IEventService
+{
+    Task<IEnumerable<EventIndexViewModel>> GetAllAsync();
+
+    Task<EventDetailsViewModel?> GetDetailsAsync(int id);
+
+    Task<EventCreateViewModel> GetCreateModelAsync();
+
+    Task<EventEditViewModel?> GetForEditAsync(int id);
+
+    Task CreateAsync(EventCreateViewModel model);
+
+    Task EditAsync(EventEditViewModel model);
+
+    Task DeleteAsync(int id);
+}
