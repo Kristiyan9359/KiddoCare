c2ad1a5ea478c7ff0f734062894c3177422645ef
Kristiyan9359 <kristiyankamboshev@gmail.com>
2026-07-29 14:56:44 +0300

    Add parents controller

---
+++ KiddoCare.Web/Controllers/ParentsController.cs
@@ -0,0 +1,130 @@
+﻿using KiddoCare.Services.Core.Contracts;
+using KiddoCare.ViewModels.Parents;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using static KiddoCare.Common.RoleConstants;
+
+namespace KiddoCare.Web.Controllers;
+
+[Authorize(Roles = Admin)]
+public class ParentsController : Controller
+{
+    private readonly IParentService parentService;
+
+    public ParentsController(IParentService parentService)
+    {
+        this.parentService = parentService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Index()
+    {
+        var parents = await parentService.GetAllAsync();
+
+        return View(parents);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Details(int id)
+    {
+        var model = await parentService.GetDetailsAsync(id);
+
+        if (model == null)
+        {
+            return NotFound();
+        }
+
+        return View(model);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Create()
+    {
+        var model = await parentService.GetCreateModelAsync();
+
+        return View(model);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(ParentCreateViewModel model)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(model);
+        }
+
+        try
+        {
+            await parentService.CreateAsync(model);
+        }
+        catch (InvalidOperationException ex)
+        {
+            ModelState.AddModelError(string.Empty, ex.Message);
+            return View(model);
+        }
+
+        return RedirectToAction(nameof(Index));
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Edit(int id)
+    {
+        var model = await parentService.GetForEditAsync(id);
+
+        if (model == null)
+        {
+            return NotFound();
+        }
+
+        return View(model);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Edit(ParentEditViewModel model)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(model);
+        }
+
+        try
+        {
+            await parentService.EditAsync(model);
+        }
+        catch (InvalidOperationException)
+        {
+            return NotFound();
+        }
+
+        return RedirectToAction(nameof(Index));
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Delete(int id)
+    {
+        var model = await parentService.GetForDeleteAsync(id);
+
+        if (model == null)
+        {
+            return NotFound();
+        }
+
+        return View(model);
+    }
+
+    [HttpPost]
+    [ActionName("Delete")]
+    public async Task<IActionResult> DeleteConfirmed(int id)
+    {
+        try
+        {
+            await parentService.DeleteAsync(id);
+        }
+        catch (InvalidOperationException)
+        {
+            return NotFound();
+        }
+
+        return RedirectToAction(nameof(Index));
+    }
+}
