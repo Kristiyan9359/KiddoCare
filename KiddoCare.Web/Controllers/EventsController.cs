7606937d3b1957261830a6b14f4e65362fffbd02
Kristiyan9359 <kristiyankamboshev@gmail.com>
2026-07-29 14:22:50 +0300

    Add events controller

---
+++ KiddoCare.Web/Controllers/EventsController.cs
@@ -0,0 +1,122 @@
+﻿using KiddoCare.Services.Core.Contracts;
+using KiddoCare.ViewModels.Events;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using static KiddoCare.Common.RoleConstants;
+
+namespace KiddoCare.Web.Controllers;
+
+[Authorize]
+public class EventsController : Controller
+{
+    private readonly IEventService eventService;
+
+    public EventsController(IEventService eventService)
+    {
+        this.eventService = eventService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Index()
+    {
+        var events = await eventService.GetAllAsync();
+
+        return View(events);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Details(int id)
+    {
+        var model = await eventService.GetDetailsAsync(id);
+
+        if (model == null)
+        {
+            return NotFound();
+        }
+
+        return View(model);
+    }
+
+    [Authorize(Roles = $"{Admin},{Teacher}")]
+    [HttpGet]
+    public async Task<IActionResult> Create()
+    {
+        var model = await eventService.GetCreateModelAsync();
+
+        return View(model);
+    }
+
+    [Authorize(Roles = $"{Admin},{Teacher}")]
+    [HttpPost]
+    public async Task<IActionResult> Create(EventCreateViewModel model)
+    {
+        if (!ModelState.IsValid)
+        {
+            model.Groups = (await eventService.GetCreateModelAsync()).Groups;
+            return View(model);
+        }
+
+        await eventService.CreateAsync(model);
+
+        return RedirectToAction(nameof(Index));
+    }
+
+    [Authorize(Roles = $"{Admin},{Teacher}")]
+    [HttpGet]
+    public async Task<IActionResult> Edit(int id)
+    {
+        var model = await eventService.GetForEditAsync(id);
+
+        if (model == null)
+        {
+            return NotFound();
+        }
+
+        return View(model);
+    }
+
+    [Authorize(Roles = $"{Admin},{Teacher}")]
+    [HttpPost]
+    public async Task<IActionResult> Edit(EventEditViewModel model)
+    {
+        if (!ModelState.IsValid)
+        {
+            var editModel = await eventService.GetForEditAsync(model.Id);
+
+            if (editModel == null)
+            {
+                return NotFound();
+            }
+
+            model.Groups = editModel.Groups;
+            return View(model);
+        }
+
+        try
+        {
+            await eventService.EditAsync(model);
+        }
+        catch (InvalidOperationException)
+        {
+            return NotFound();
+        }
+
+        return RedirectToAction(nameof(Index));
+    }
+
+    [Authorize(Roles = $"{Admin},{Teacher}")]
+    [HttpPost]
+    public async Task<IActionResult> Delete(int id)
+    {
+        try
+        {
+            await eventService.DeleteAsync(id);
+        }
+        catch (InvalidOperationException)
+        {
+            return NotFound();
+        }
+
+        return RedirectToAction(nameof(Index));
+    }
+}
