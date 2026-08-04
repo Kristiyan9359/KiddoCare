c91552fdd14f13dc7e37ad2e0bae808d54874a9f
Kristiyan9359 <kristiyankamboshev@gmail.com>
2026-08-04 18:03:35 +0300

    Add activity feed view models

---
+++ KiddoCare.ViewModels/ActivityFeed/ActivityFeedItemViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace KiddoCare.ViewModels.ActivityFeed;
+
+public class ActivityFeedItemViewModel
+{
+    public DateTime Date { get; set; }
+
+    public string Type { get; set; } = null!;
+
+    public string Title { get; set; } = null!;
+
+    public string? Description { get; set; }
+
+    public string? ActionController { get; set; }
+
+    public string? ActionName { get; set; }
+
+    public int? RouteId { get; set; }
+}
---
+++ KiddoCare.ViewModels/ActivityFeed/ChildActivityFeedViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace KiddoCare.ViewModels.ActivityFeed;
+
+public class ChildActivityFeedViewModel
+{
+    public int ChildId { get; set; }
+
+    public string ChildFullName { get; set; } = null!;
+
+    public IEnumerable<ActivityFeedItemViewModel> Items { get; set; } = new List<ActivityFeedItemViewModel>();
+}
