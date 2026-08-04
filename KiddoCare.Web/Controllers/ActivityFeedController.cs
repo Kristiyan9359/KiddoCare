f91854f09e4a3bc8d52f2863dbccd7232754eb04
Kristiyan9359 <kristiyankamboshev@gmail.com>
2026-08-04 18:04:34 +0300

    Add activity feed controller

---
+++ KiddoCare.Web/Controllers/ActivityFeedController.cs
@@ -0,0 +1,35 @@
+﻿using System.Security.Claims;
+using KiddoCare.Services.Core.Contracts;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using static KiddoCare.Common.RoleConstants;
+
+namespace KiddoCare.Web.Controllers;
+
+[Authorize]
+public class ActivityFeedController : Controller
+{
+    private readonly IActivityFeedService activityFeedService;
+
+    public ActivityFeedController(IActivityFeedService activityFeedService)
+    {
+        this.activityFeedService = activityFeedService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Child(int childId)
+    {
+        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier)!;
+        var isAdmin = User.IsInRole(Admin);
+        var isTeacher = User.IsInRole(Teacher);
+
+        var model = await activityFeedService.GetChildFeedAsync(childId, userId, isAdmin, isTeacher);
+
+        if (model == null)
+        {
+            return NotFound();
+        }
+
+        return View(model);
+    }
+}
