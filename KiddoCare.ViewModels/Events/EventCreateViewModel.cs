89d5c73bf5365e8e860dbc71d30a013e3988e946
Kristiyan9359 <kristiyankamboshev@gmail.com>
2026-08-12 19:03:41 +0300

    Polish event pages

--- KiddoCare.ViewModels/Events/EventCreateViewModel.cs
+++ KiddoCare.ViewModels/Events/EventCreateViewModel.cs
@@ -7,26 +7,34 @@ namespace KiddoCare.ViewModels.Events;
 
 public class EventCreateViewModel
 {
-    [Required]
-    [MaxLength(EventTitleMaxLength)]
+    [Display(Name = "Event title")]
+    [Required(ErrorMessage = "Please enter an event title.")]
+    [MaxLength(EventTitleMaxLength, ErrorMessage = "Event title cannot be longer than {1} characters.")]
     public string Title { get; set; } = null!;
 
-    [MaxLength(EventDescriptionMaxLength)]
+    [Display(Name = "Description")]
+    [MaxLength(EventDescriptionMaxLength, ErrorMessage = "Description cannot be longer than {1} characters.")]
     public string? Description { get; set; }
 
-    [Required]
+    [Display(Name = "Starts on")]
+    [Required(ErrorMessage = "Please select when the event starts.")]
     public DateTime StartDateTime { get; set; } = DateTime.Now.AddDays(1);
 
+    [Display(Name = "Ends on")]
     public DateTime? EndDateTime { get; set; }
 
-    [Required]
+    [Display(Name = "Event type")]
+    [Required(ErrorMessage = "Please select an event type.")]
     public EventType Type { get; set; } = EventType.General;
 
-    [MaxLength(EventLocationMaxLength)]
+    [Display(Name = "Location")]
+    [MaxLength(EventLocationMaxLength, ErrorMessage = "Location cannot be longer than {1} characters.")]
     public string? Location { get; set; }
 
+    [Display(Name = "Group")]
     public int? GroupId { get; set; }
 
+    [Display(Name = "Visible to parents")]
     public bool IsPublic { get; set; } = true;
 
     public IEnumerable<SelectListItem> Groups { get; set; } = new List<SelectListItem>();
--- KiddoCare.ViewModels/Events/EventEditViewModel.cs
+++ KiddoCare.ViewModels/Events/EventEditViewModel.cs
@@ -9,26 +9,34 @@ public class EventEditViewModel
 {
     public int Id { get; set; }
 
-    [Required]
-    [MaxLength(EventTitleMaxLength)]
+    [Display(Name = "Event title")]
+    [Required(ErrorMessage = "Please enter an event title.")]
+    [MaxLength(EventTitleMaxLength, ErrorMessage = "Event title cannot be longer than {1} characters.")]
     public string Title { get; set; } = null!;
 
-    [MaxLength(EventDescriptionMaxLength)]
+    [Display(Name = "Description")]
+    [MaxLength(EventDescriptionMaxLength, ErrorMessage = "Description cannot be longer than {1} characters.")]
     public string? Description { get; set; }
 
-    [Required]
+    [Display(Name = "Starts on")]
+    [Required(ErrorMessage = "Please select when the event starts.")]
     public DateTime StartDateTime { get; set; }
 
+    [Display(Name = "Ends on")]
     public DateTime? EndDateTime { get; set; }
 
-    [Required]
+    [Display(Name = "Event type")]
+    [Required(ErrorMessage = "Please select an event type.")]
     public EventType Type { get; set; }
 
-    [MaxLength(EventLocationMaxLength)]
+    [Display(Name = "Location")]
+    [MaxLength(EventLocationMaxLength, ErrorMessage = "Location cannot be longer than {1} characters.")]
     public string? Location { get; set; }
 
+    [Display(Name = "Group")]
     public int? GroupId { get; set; }
 
+    [Display(Name = "Visible to parents")]
     public bool IsPublic { get; set; }
 
     public IEnumerable<SelectListItem> Groups { get; set; } = new List<SelectListItem>();
